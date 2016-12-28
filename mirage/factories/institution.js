29a7eaa378cacd00ac75549b0eee844081d221a6
Buck Doyle <b@chromatin.ca>
2016-12-27 21:26:42 -0500

    Change to real institutions names for example data

--- mirage/factories/institution.js
+++ mirage/factories/institution.js
@@ -1,5 +1,4 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  name() { return faker.company.companyName(); }
 });
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -2,7 +2,19 @@ export default function(server) {
   const driver = server.create('person', {name: 'Driver'});
   const carOwner = server.create('person', {name: 'Car Owner'});
 
+  const institutions = [
+    'Brandon',
+    'Headingley',
+    'Milner Ridge',
+    'Rockwood',
+    'Stony Mountain'
+  ];
+
   for (let i = 0; i < 5; i++) {
-    server.create('ride', { institution: server.create('institution'), driver, carOwner });
+    const institution = server.create('institution', {
+      name: institutions[i]
+    });
+
+    server.create('ride', { institution, driver, carOwner });
   }
 }
