141d61ca973550420caba31192e6120234f1f4eb
Buck Doyle <b@chromatin.ca>
2017-01-13 22:35:44 -0500

    Change session display to logout button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+
+  actions: {
+    logout() {
+      this.get('session').invalidate();
+      this.store.unloadAll();
+    }
+  }
 });
