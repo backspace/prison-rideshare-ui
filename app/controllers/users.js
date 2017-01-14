9c21ff8ca329b42425b5e0f0876011022a847c47
Buck Doyle <b@chromatin.ca>
2017-01-13 23:03:56 -0500

    Add blocking of self-de-admining

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+
   actions: {
     updateUserAdmin(user, admin) {
       user.set('admin', admin);
