6a9a1a2133749cf03029c54a0d72990e12f547c7
Buck Doyle <b@chromatin.ca>
2017-03-19 15:02:00 -0700

    Remove obsolete properties

--- app/models/reimbursement.js
+++ app/models/reimbursement.js
@@ -2,16 +2,11 @@ import DS from 'ember-data';
 import dollars from 'prison-rideshare-ui/utils/dollars';
 
 export default DS.Model.extend({
-  // FIXME this is going away!
-  amount: DS.attr('number'),
-
   foodExpenses: DS.attr('number', {defaultValue: 0}),
   carExpenses: DS.attr('number', {defaultValue: 0}),
 
   person: DS.belongsTo(),
   donation: DS.attr('boolean'),
-
-  amountDollars: dollars('amount'),
 
   foodExpensesDollars: dollars('foodExpenses'),
   carExpensesDollars: dollars('carExpenses')
