ead40d3ace3644078291fafe83721e5b605d3144
Buck Doyle <b@chromatin.ca>
2017-03-19 17:54:00 -0700

    Change unauthenticated root forwarding destination

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRoute from 'prison-rideshare-ui/mixins/authenticated-route';
 
 export default Ember.Route.extend(AuthenticatedRoute, {
+  session: Ember.inject.service(),
+
   beforeModel() {
-    this.transitionTo('rides');
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('rides');
+    } else {
+      this.transitionTo('reports.new');
+    }
   }
 });
