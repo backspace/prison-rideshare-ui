ed990854c7cb455dfc517e3b6c5f5f17665c8056
Buck Doyle <b@chromatin.ca>
2017-03-20 22:14:01 -0700

    Add ability to submit registration with keyboard

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -4,9 +4,10 @@ export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
   actions: {
-    register() {
-      const user = this.get('model');
+    register(event) {
+      event.preventDefault();
 
+      const user = this.get('model');
       user.save();
     }
   }
