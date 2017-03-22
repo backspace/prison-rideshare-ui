5744d4e251e9fd3948b62029aef335e93ea79ac0
Buck Doyle <b@chromatin.ca>
2017-03-21 22:59:20 -0700

    Add sandbox login information

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
+  sandbox: Ember.computed(function() {
+    return window.location.hostname.indexOf('sandbox') > -1;
+  }),
+
   actions: {
     login(event) {
       event.preventDefault();
