715569c97970d449ad8801815726ae7c82a58e71
Buck Doyle <b@chromatin.ca>
2017-04-15 15:35:47 -0700

    Add header indication of sandbox
    This is a gross hack to repurpose chips but I don’t care
    at this point 😬

--- app/components/toolbar-header.js
+++ app/components/toolbar-header.js
@@ -4,6 +4,19 @@ export default Ember.Component.extend({
   sidebar: Ember.inject.service(),
   sidebarOpen: Ember.computed.alias('sidebar.open'),
 
+  chips: Ember.computed(function() {
+    const hostname = window.location.hostname;
+
+    if (hostname.indexOf('sandbox') > -1) {
+      return [{
+        label: 'Sandbox',
+        title: 'All data on this instance is erased daily. If some type of example data would be useful for you, let Buck know.'
+      }];
+    } else {
+      return [];
+    }
+  }),
+
   actions: {
     toggleSidebar() {
       this.toggleProperty('sidebarOpen');
