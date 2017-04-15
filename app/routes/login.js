0df3f8109f4fb6cb4ad027010e46bdb56df1bc92
Buck Doyle <b@chromatin.ca>
2017-04-15 15:12:07 -0700

    Remove unnecessary use of user record
    This was producing an extra entry in the users list for
    the just-logged-in user. It seems pointless to me to use
    the user model for login. No test but ¯\_(ツ)_/¯

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,7 +3,7 @@ import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   model() {
-    return this.store.createRecord('user');
+    return Ember.Object.create();
   },
 
   titleToken: 'Log in'
