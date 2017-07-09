a6a12e7ab60ee8148ed3d9d39c1a7a1220710ccb
Buck Doyle <b@chromatin.ca>
2017-07-09 14:37:08 -0400

    Change no driver cancellation reason icon

--- app/utils/reason-to-icon.js
+++ app/utils/reason-to-icon.js
@@ -2,8 +2,7 @@ const reasonToIcon = {
   'lockdown': 'lock',
   'visitor': 'perm identity',
   'no car': 'directions car',
-  // FIXME better icon!
-  'no driver': 'rowing',
+  'no driver': 'assignment ind',
   'weather': 'cloud',
   'transfer': 'shuffle',
   'error': 'error',
