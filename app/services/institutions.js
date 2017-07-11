f9fa65eabebaa4be12f90953fcbdd482dcdcc695
Buck Doyle <b@chromatin.ca>
2017-07-10 20:37:26 -0400

    Add default alphabetic sorting of institutions

--- app/services/institutions.js
+++ app/services/institutions.js
@@ -4,6 +4,6 @@ export default Ember.Service.extend({
   store: Ember.inject.service(),
 
   all: Ember.computed(function() {
-    return this.get('store').findAll('institution');
+    return this.get('store').findAll('institution').then(institutions => institutions.sortBy('name'));
   })
 });
