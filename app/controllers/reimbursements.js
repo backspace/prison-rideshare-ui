b228d8ab13afb2531fbd8bbabd681ea8a98ece9d
Buck Doyle <b@chromatin.ca>
2017-09-22 11:44:06 +0200

    Add ability to copy values from reimbursements
    This makes it easier for working with the ledger. It’s shamefully
    lacking in automated testing, I guess it’s not possible to access
    the contents of the clipboard, sensibly enough. I could extract
    the ReimbursementCollection for testing but I can’t bring myself
    to do it at the moment.

--- app/controllers/reimbursements.js
+++ app/controllers/reimbursements.js
@@ -4,6 +4,8 @@ import Ember from 'ember';
 import sum from 'ember-cpm/macros/sum';
 import dollars from 'prison-rideshare-ui/utils/dollars';
 
+import moment from 'moment';
+
 export default Ember.Controller.extend({
   queryParams: {
     showProcessed: 'processed'
@@ -103,5 +105,27 @@ const ReimbursementCollection = Ember.Object.extend({
   carExpensesDollars: dollars('carExpensesSum'),
 
   totalExpenses: sum('foodExpensesSum', 'carExpensesSum'),
-  totalExpensesDollars: dollars('totalExpenses')
+  totalExpensesDollars: dollars('totalExpenses'),
+
+  clipboardText: Ember.computed('person.name', 'totalExpensesDollars', function() {
+    const name = this.get('person.name');
+    const total = this.get('totalExpensesDollars');
+
+    const today = new Date();
+    const dateString = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+
+    const lastMonthName = moment(today).subtract(1, 'months').format('MMMM');
+
+    return `${dateString}\t` +
+      `${lastMonthName} mileage${this.get('foodExpensesSum') > 0 ? ' + meal' : ''}\t` +
+      `${name}\t` +
+      `-$${total}\t` +
+      `${this.get('donations') ? `$${total}` : ''}\t` +
+      '\t' +
+      `${this.get('donations') ? '(donated)' : ''}`;
+  }),
+
+  copyIconTitle: Ember.computed('clipboardText', function() {
+    return `This will copy the following to the clipboard: ${this.get('clipboardText')}`;
+  })
 });
