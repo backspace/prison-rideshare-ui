6044a92f3e01e086cb74c7c8017e171fe6771a93
Buck Doyle <b@chromatin.ca>
2017-10-13 10:32:06 -0700

    Remove unused test setup
    Copy-paste error!

--- tests/acceptance/users-test.js
+++ tests/acceptance/users-test.js
@@ -107,9 +107,5 @@ test('shows who is present', function(assert) {
 
   andThen(() => {
     assert.ok(shared.userCount.isHidden, 'expected the count to not include duplicates');
-
-    const rejoinPresenceDiffMessage = {joins: {}, leaves: {}};
-    rejoinPresenceDiffMessage.joins[`User:${this.admin.id}`] = {};
-    userSocket._onPresenceDiff(rejoinPresenceDiffMessage);
   });
 });
