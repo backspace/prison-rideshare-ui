269e92d0484d4bec32e25d8ca3fd0b015fbe9d09
Buck Doyle <b@chromatin.ca>
2017-11-11 16:58:18 -0600

    Change deployment branch

--- .travis/deploy-sandbox.sh
+++ .travis/deploy-sandbox.sh
@@ -6,4 +6,4 @@ ssh-add .travis/deploy.key
 ssh-keyscan corepoint.chromatin.ca >> ~/.ssh/known_hosts
 git remote add deploy dokku@corepoint.chromatin.ca:prison-rideshare-sandbox
 git config --global push.default simple
-git push deploy dokku:primary
+git push deploy primary
