b53f3fa41a1731b126d5fa3620523f20f884582c
Buck Doyle <b@chromatin.ca>
2017-12-02 13:33:00 -0600

    Add attempt at generating PR deployments

---
+++ .travis/deploy-pull-request.sh
@@ -0,0 +1,11 @@
+export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' | tr '[:upper:]' '[:lower:]'`
+
+export API_HOST=https://prison-rideshare-api.corepoint.chromatin.ca/
+export SOCKET_HOST=wss://prison-rideshare-api.corepoint.chromatin.ca
+ember deploy production --activate
+DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=production ./.travis/deployment/update-github-status.sh
+
+export API_HOST=https://prison-rideshare-api-sandbox.corepoint.chromatin.ca/
+export SOCKET_HOST=wss://prison-rideshare-api-sandbox.corepoint.chromatin.ca
+ember deploy sandbox --activate
+DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=sandbox ./.travis/deployment/update-github-status.sh
---
+++ .travis/update-github-status.sh
@@ -0,0 +1,18 @@
+echo env = $ENVIRONMENT and tld = $TLD
+
+FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN`.test-deployments.barnonewpg.org
+
+echo deployment url: $FULL_URL
+
+if [[ $DEPLOYMENT_EXIT_CODE -eq 0 ]]
+then
+  curl -X POST \
+       --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$ENVIRONMENT\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+else
+  curl -X POST \
+       --data "{\"state\": \"error\", \"description\": \"There was a failure with the PR deployment\", \"context\": \"deployments/$ENVIRONMENT\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+fi
