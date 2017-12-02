2041eb0ddf5643de66dcc8c835ad09a551316b26
Buck Doyle <b@chromatin.ca>
2017-12-02 14:00:56 -0600

    Correct script paths

--- .travis/deploy-pull-request.sh
+++ .travis/deploy-pull-request.sh
@@ -5,9 +5,9 @@ export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' |
 export API_HOST=https://prison-rideshare-api.corepoint.chromatin.ca/
 export SOCKET_HOST=wss://prison-rideshare-api.corepoint.chromatin.ca
 ember deploy production --activate
-DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=production ./.travis/deployment/update-github-status.sh
+DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=production ./.travis/update-github-status.sh
 
 export API_HOST=https://prison-rideshare-api-sandbox.corepoint.chromatin.ca/
 export SOCKET_HOST=wss://prison-rideshare-api-sandbox.corepoint.chromatin.ca
 ember deploy sandbox --activate
-DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=sandbox ./.travis/deployment/update-github-status.sh
+DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=sandbox ./.travis/update-github-status.sh
