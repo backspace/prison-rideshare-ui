1b9f7b283a52d8200df0e2b96b8088efb15f565c
Buck Doyle <b@chromatin.ca>
2017-12-03 11:08:11 -0600

    Remove npx prefix

--- .travis/deploy-sandbox.sh
+++ .travis/deploy-sandbox.sh
@@ -2,4 +2,4 @@ KEY_FILENAME=deploy-lightning.key ./.travis/prepare-ssh-key.sh
 
 export API_HOST=https://prison-rideshare-api-sandbox.corepoint.chromatin.ca/
 export SOCKET_HOST=wss://prison-rideshare-api-sandbox.corepoint.chromatin.ca
-npx ember deploy sandbox --activate
+ember deploy sandbox --activate
