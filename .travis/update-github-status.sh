15d7f7dcfb9bbdeb56ea05ca7bccf43d916601df
Buck Doyle <b@chromatin.ca>
2017-12-03 10:34:48 -0600

    Add domain commands after deployment
    I think this won’t work but we’ll see!

--- .travis/update-github-status.sh
+++ .travis/update-github-status.sh
@@ -1,11 +1,15 @@
 echo env = $ENVIRONMENT and tld = $TLD
 
-FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN`.test-deployments.barnonewpg.org
+HOST=`echo $CLEANED_BRANCH_SUBDOMAIN`.test-deployments.barnonewpg.org
+FULL_URL=https://`echo $HOST`
 
 echo deployment url: $FULL_URL
 
 if [[ $DEPLOYMENT_EXIT_CODE -eq 0 ]]
 then
+  ssh -t dokku@corepoint.chromatin.ca -- run rideshare-lightning dokku domains:add rideshare-lightning $HOST
+  ssh -t dokku@corepoint.chromaitn.ca -- run rideshare-lightning dokku letsencrypt rideshare-lightning
+
   curl -X POST \
        --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$ENVIRONMENT\"}" \
        -H "Authorization: token $GITHUB_TOKEN" \
