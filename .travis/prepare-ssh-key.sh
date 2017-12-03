fab8debec8ac19a88ba5ac7e6b32fa8a9149ef9f
Buck Doyle <b@chromatin.ca>
2017-12-03 13:21:22 -0600

    Apply chmod to entire directory
    ¯\_(ツ)_/¯

--- .travis/prepare-ssh-key.sh
+++ .travis/prepare-ssh-key.sh
@@ -1,6 +1,6 @@
 openssl aes-256-cbc -K $encrypted_ce91012c77dd_key -iv $encrypted_ce91012c77dd_iv -in .travis/keys.tar.enc -out .travis/keys.tar -d
 tar xfv .travis/keys.tar
-chmod 600 .travis/*.key
+chmod -R 600 .travis
 
 eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add .travis/$KEY_FILENAME
