adcb96b123ee64b4711079af85982ff4dddffdf6
Buck Doyle <b@chromatin.ca>
2017-12-03 10:20:19 -0600

    Add npx prefix
    Why???

--- .travis/deploy-sandbox.sh
+++ .travis/deploy-sandbox.sh
@@ -2,4 +2,4 @@ KEY_FILENAME=deploy-lightning.key ./.travis/prepare-ssh-key.sh
 
 export API_HOST=https://prison-rideshare-api-sandbox.corepoint.chromatin.ca/
 export SOCKET_HOST=wss://prison-rideshare-api-sandbox.corepoint.chromatin.ca
-ember deploy sandbox --activate
+npx ember deploy sandbox --activate
