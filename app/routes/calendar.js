48ebcfacbbab704f77a1303d76e94dbaece06aff
Buck Doyle <b@chromatin.ca>
2018-01-05 05:17:05 -0600

    Remove fake slots

--- app/routes/calendar.js
+++ app/routes/calendar.js
@@ -37,11 +37,7 @@ export default Route.extend({
       throw new Error('We were unable to log you in with that token.');
     }).then(person => {
       return RSVP.hash({
-        slots: this.store.findAll('slot').catch(() => [
-          this.store.createRecord('slot', {start: new Date(2017, 11, 3, 17), end: new Date(2017, 11, 3, 21), count: 2}),
-          this.store.createRecord('slot', {start: new Date(2017, 11, 3, 11), end: new Date(2017, 11, 3, 17), count: 3}),
-          this.store.createRecord('slot', {start: new Date(2017, 11, 8, 17), count: 4})
-        ]),
+        slots: this.store.findAll('slot'),
         person,
         month
       });
