e7fb928338ec9fd415d0760194649e8f3a647ee0
Buck Doyle <b@chromatin.ca>
2018-01-22 18:49:42 -0600

    Add short-meridiem override for calendar
    That the tests don’t need to be changed reveals something!

--- app/utils/format-brief-timespan.js
+++ app/utils/format-brief-timespan.js
@@ -4,6 +4,18 @@ function isAnteMeridiem(time) {
   return time.getHours() < 12;
 }
 
+// FIXME this is duplicated in format-timespan
+moment.updateLocale('en', {
+  meridiem(hours, minutes, isLower) {
+    // Taken from https://github.com/moment/moment/blob/7785323888893428f08eb6d5dc5eb266d5bf2a11/src/lib/units/hour.js#L130
+    if (hours > 11) {
+      return isLower ? 'p' : 'P';
+    } else {
+      return isLower ? 'a' : 'A';
+    }
+  }
+});
+
 export default function formatTimespan(start, end) {
   let startTime, endTime;
 
