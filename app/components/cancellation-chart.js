c751b88a789e50cc4b8e761c7c128f9bbc5e7044
Buck Doyle <b@chromatin.ca>
2018-01-24 14:58:22 +0100

    Convert to stacked chart with cancellation reasons

--- app/components/cancellation-chart.js
+++ app/components/cancellation-chart.js
@@ -1,27 +1,69 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 
+import reasonToIcon from 'prison-rideshare-ui/utils/reason-to-icon';
+
+const reasons = Object.keys(reasonToIcon);
+
 export default Component.extend({
-  data: computed('rides.@each.start', function() {
-    const rides = this.get('rides');
-    const cancelled = rides.filterBy('cancelled');
-    return [{
-      name: 'Requests',
-      data: [cancelled.length, rides.length - cancelled.length]
-    }];
+  reasonToCount: computed('rides.@each.cancellationReason', function() {
+    return this.get('rides').reduce((reasonToCount, ride) => {
+      let key;
+
+      if (ride.get('cancelled')) {
+        const reason = ride.get('cancellationReason')
+        if (reasons.includes(reason)) {
+          key = reason;
+        } else {
+          key = 'other';
+        }
+      } else {
+        key = 'not cancelled';
+      }
+
+      if (!reasonToCount[key]) {
+        reasonToCount[key] = 0;
+      }
+
+      reasonToCount[key]++;
+
+      return reasonToCount;
+    }, {})
   }),
-  options: Object.freeze({
-    chart: {
-      type: 'bar',
-    },
-    title: {
-      text: 'Cancellations'
-    },
-    xAxis: {
-      categories: ['Cancelled', 'Not cancelled']
-    },
-    yAxis: {
-      categories: ['Requests'],
-    },
+  data: computed('reasonToCount', function() {
+    const reasonToCount = this.get('reasonToCount');
+    return Object.keys(reasonToCount).map(key => {
+        if (key === 'not cancelled') {
+          return {
+            name: key,
+            data: [reasonToCount[key], 0]
+          };
+        } else {
+          return {
+            name: key,
+            data: [0, reasonToCount[key]]
+          };
+        }
+      });
+  }),
+  options: computed('reasonToCount', function() {
+    return {
+      chart: {
+        type: 'bar',
+      },
+      title: {
+        text: 'Cancellations'
+      },
+      plotOptions: {
+        series: {
+          stacking: 'normal'
+        }
+      },
+      xAxis: {
+        categories: ['Not cancelled', 'Cancelled']
+      },
+      yAxis: {
+      },
+    };
   }),
 });
