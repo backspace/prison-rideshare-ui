3cfab0c527dff1334b09600455cfae11f60404f0
Buck Doyle <b@chromatin.ca>
2018-01-24 02:47:16 +0100

    Fix typo

--- tests/acceptance/statistics-test.js
+++ tests/acceptance/statistics-test.js
@@ -30,7 +30,7 @@ test('shows ride start times per day, with a default range of the past year', fu
     assert.equal(shared.title, 'Statistics · Prison Rideshare');
 
     assert.equal(page.start.value, '2017-01-24', 'expected the start date to be a year ago');
-    assert.equal(page.end.value, '2018-01-24', 'expected the end date to be a year ago');
+    assert.equal(page.end.value, '2018-01-24', 'expected the end date to be today');
 
     assert.equal(page.times.days().count, 8, 'expected seven day rows and the header');
     assert.equal(page.times.days(2).hours(10).text, '1');
