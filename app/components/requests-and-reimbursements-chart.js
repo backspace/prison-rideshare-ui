782873657578330af6d9dcb4d7ece4ff363b4d44
Buck Doyle <b@chromatin.ca>
2018-01-24 20:07:10 +0100

    Add grouping control for combined graph

--- app/components/requests-and-reimbursements-chart.js
+++ app/components/requests-and-reimbursements-chart.js
@@ -4,7 +4,9 @@ import { computed } from '@ember/object';
 import moment from 'moment';
 
 export default Component.extend({
-  timeGroups: computed('rides.@each.start', function() {
+  grouping: 'months',
+
+  timeGroups: computed('rides.@each.start', 'grouping', function() {
     return this.get('rides').reduce((timeGroups, ride) => {
       const timeGroupForRide = this.timeGroupForRide(ride);
 
@@ -18,13 +20,18 @@ export default Component.extend({
     }, {});
   }),
 
-  timeGroupKeys: computed('timeGroups', function() {
+  timeGroupKeys: computed('timeGroups', 'grouping', function() {
     return Object.keys(this.get('timeGroups')).sort()
   }),
 
   timeGroupForRide(ride) {
     const start = ride.get('start');
-    return moment(start).format('YYYY-MM');
+
+    if (this.get('grouping') === 'weeks') {
+      return moment(start).startOf('week').format('YYMMDD');
+    } else {
+      return moment(start).format('YYYY-MM');
+    }
   },
 
   data: computed('timeGroups', function() {
@@ -39,7 +46,7 @@ export default Component.extend({
     }, {
       name: 'Distance',
       type: 'spline',
-      yAxis: 2,
+      yAxis: 1,
       data: this.get('timeGroupKeys').map(timeGroupKey => {
         return timeGroups[timeGroupKey].reduce((sum, ride) => {
           return sum + (ride.get('distance') || 0);
@@ -48,7 +55,7 @@ export default Component.extend({
       }, {
         name: 'Reimbursements',
         type: 'spline',
-        yAxis: 1,
+        yAxis: 2,
         data: this.get('timeGroupKeys').map(timeGroupKey => {
           return timeGroups[timeGroupKey].reduce((sum, ride) => {
             return sum + ride.get('reimbursementFoodExpensesSum') + ride.get('reimbursementCarExpensesSum');
@@ -57,7 +64,7 @@ export default Component.extend({
       }, {
         name: 'Food expenses',
         type: 'spline',
-        yAxis: 1,
+        yAxis: 2,
         data: this.get('timeGroupKeys').map(timeGroupKey => {
           return timeGroups[timeGroupKey].reduce((sum, ride) => {
             return sum + ride.get('reimbursementFoodExpensesSum');
@@ -67,7 +74,7 @@ export default Component.extend({
     ];
   }),
 
-  options: computed('timeGroups', function() {
+  options: computed('timeGroups', 'timeGroupKeys.length', function() {
     return {
       title: {
         text: 'Period-grouped requests and reimbursements'
