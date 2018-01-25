a51cd225292a29e1da27949d072dc5c20aec700b
Buck Doyle <b@chromatin.ca>
2018-01-25 04:48:10 +0100

    Update time chart to show span of visit
    I think this would be even better split more finely, but I’m
    too tired to think through it at the moment.

--- app/components/request-time-chart.js
+++ app/components/request-time-chart.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 export default Component.extend({
   data: computed('rides.@each.start', function() {
     const data = this.get('rides').reduce((days, ride) => {
-      const start = ride.get('start');
+      const start = ride.get('start'), end  = ride.get('end');
       const startInTimeZone = moment.tz(start, 'America/Winnipeg');
       // const day = (startInTimeZone.day() - 1) % 7;
       const day = startInTimeZone.day() ? startInTimeZone.day() - 1 : 6;
@@ -14,13 +14,19 @@ export default Component.extend({
         days[day] = {hours: new Array(24), name: startInTimeZone.format('ddd'), day};
       }
 
-      const hour = startInTimeZone.hour();
+      let currentHour = startInTimeZone.startOf('hour');
 
-      if (!days[day].hours[hour]) {
-        days[day].hours[hour] = 0;
+      while (currentHour.isBefore(end)) {
+        const hour = currentHour.hour();
+
+        if (!days[day].hours[hour]) {
+          days[day].hours[hour] = 0;
+        }
+
+        days[day].hours[hour] += 1;
+
+        currentHour = currentHour.add(1, 'hour');
       }
-
-      days[day].hours[hour] += 1;
 
       return days;
     }, new Array(7)).reduce((data, day, index) => {
@@ -46,7 +52,7 @@ export default Component.extend({
       plotBorderWidth: 1
     },
     title: {
-      text: 'Request start times by day'
+      text: 'Visit times by day'
     },
     xAxis: {
       categories: Array(24).fill().map((empty, index) => index)
@@ -75,7 +81,7 @@ export default Component.extend({
         let x = this.series.xAxis.categories[this.point.x];
         let y = this.series.yAxis.categories[this.point.y];
         let { value } = this.point;
-        return `<strong>${value}</strong> request${value > 1 ? 's' : ''} starting at <strong>${x}h</strong> on <strong>${y}</strong>`;
+        return `<strong>${value}</strong> request${value > 1 ? 's' : ''} during <strong>${x}h</strong> on <strong>${y}s</strong>`;
       }
     }
   }),
