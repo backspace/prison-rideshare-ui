0533d2b38d46cd901601e1942965e6636abf5d66
Buck Doyle <b@chromatin.ca>
2018-05-18 23:04:15 -0500

    Convert LinkedContact
    There’s a Typescript error that Handlebars.Utils doesn’t exist…?

--- app/components/linked-contact.ts
+++ app/components/linked-contact.ts
@@ -3,18 +3,19 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
 
-export default Component.extend({
-  tagName: 'span',
+export default class LinkedContact extends Component {
+  contact!: String;
+  tagName = 'span';
 
-  linkedContact: computed('contact', function() {
+  linkedContact = computed('contact', function() {
     const phonePattern = /(\([0-9]{3}\)\s?|[0-9]{3}-?\s?)[0-9]{3}-?\s?[0-9]{4}/g;
     const contact = this.get('contact');
 
     if (contact) {
-      return htmlSafe(contact.replace(phonePattern, function(number) {
+      return htmlSafe(contact.replace(phonePattern, function(number: String) {
         const escapedNumber = Ember.Handlebars.Utils.escapeExpression(number);
         return `<a href='tel:${escapedNumber}'>${escapedNumber}</a>`;
       }));
     }
   })
-});
+};
