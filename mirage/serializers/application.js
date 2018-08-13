ea114dbe24e493ec52b97a3dc3909e6deff4b570
Buck Doyle <b@chromatin.ca>
2018-08-12 19:38:52 -0500

    Update to latest Mirage
    I should fix the need for alwaysIncludeLinkageData butttt
    I wasn’t really trying to focus on this at the moment!

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,4 +1,6 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
 export default JSONAPISerializer.extend({
+  // FIXME http://www.ember-cli-mirage.com/docs/v0.4.x/upgrading/#03x--04-upgrade-guide
+  alwaysIncludeLinkageData: true
 });
