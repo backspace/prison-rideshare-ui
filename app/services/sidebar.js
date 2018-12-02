e0319cb46ade06ae6a316371d0afb437f403da4c
Buck Doyle <b@chromatin.ca>
2018-12-02 09:49:09 -0600

    Add overlaps count to mobile menu badge

--- app/services/sidebar.js
+++ app/services/sidebar.js
@@ -7,6 +7,7 @@ import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
 let ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
 
 export default Service.extend({
+  overlaps: service(),
   store: service(),
   userSocket: service(),
 
@@ -43,6 +44,9 @@ export default Service.extend({
   }),
 
   notificationCount: computed('userCount', 'unreadCount', function() {
-    return this.get('userCount') + this.get('unreadCount');
+    // FIXME this is untested
+    return this.get('userCount') +
+      this.get('unreadCount') +
+      (this.get('overlaps.overlaps.data.length') || 0);
   }),
 });
