056ab7ac9063bce8e7e8cfa71f99a21403e8760f
Buck Doyle <b@chromatin.ca>
2018-12-09 19:59:23 -0600

    Update API URLs for PR/sandbox deployments

--- .travis/deploy-pull-request.sh
+++ .travis/deploy-pull-request.sh
@@ -2,8 +2,8 @@
 
 export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' | tr '[:upper:]' '[:lower:]'`
 
-export API_HOST=https://prison-rideshare-api.corepoint.chromatin.ca/
-export SOCKET_HOST=wss://prison-rideshare-api.corepoint.chromatin.ca
+export API_HOST=https://rideshare-api.barnonewpg.org
+export SOCKET_HOST=wss://rideshare-api.barnonewpg.org
 ember deploy pull-request-production --activate
 DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=production ./.travis/update-github-status.sh
 
