2a6dfa712eb770f1b4ae789e6323ab6447f8c3f4
Buck Doyle <b@chromatin.ca>
2018-12-16 15:58:08 -0600

    Remove ember-simple-auth authoriser
    I thought I failed to accomplish this twice before but now
    it seems so simple… wha?

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,7 +5,14 @@ import config from 'prison-rideshare-ui/config/environment';
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.DS.host,
   namespace: config.DS.namespace,
-  authorizer: 'authorizer:application',
+
+  authorize(xhr) {
+    let { access_token } = this.get('session.data.authenticated');
+
+    if (access_token) {
+      xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+    }
+  },
 
   urlForCreateRecord(modelName) {
     switch (modelName) {
