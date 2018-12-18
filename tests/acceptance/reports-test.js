eb1598bb4403ad5480ba77050ddef54a174885db
Buck Doyle <b@chromatin.ca>
2018-12-17 22:42:30 -0600

    Convert reports tests

--- tests/acceptance/reports-test.js
+++ tests/acceptance/reports-test.js
@@ -1,22 +1,27 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'prison-rideshare-ui/tests/helpers/module-for-acceptance';
+import { currentURL } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-import { authenticateSession } from 'prison-rideshare-ui/tests/helpers/ember-simple-auth';
+import { authenticateSession } from 'ember-simple-auth/test-support';
 
 import page from 'prison-rideshare-ui/tests/pages/report';
 import shared from 'prison-rideshare-ui/tests/pages/shared';
 
-moduleForAcceptance('Acceptance | reports', {
-  beforeEach() {
-    const leavenworth = server.create('institution', {
+module('Acceptance | reports', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function() {
+    const leavenworth = this.server.create('institution', {
       name: 'Fort Leavenworth',
     });
 
-    const remand = server.create('institution', {
+    const remand = this.server.create('institution', {
       name: 'Remand Centre',
     });
 
-    const edwardRide = server.create('ride', {
+    const edwardRide = this.server.create('ride', {
       name: 'Edward',
       start: new Date(2016, 11, 27, 17, 0),
       end: new Date(2016, 11, 27, 19, 0),
@@ -25,7 +30,7 @@ moduleForAcceptance('Acceptance | reports', {
       donatable: false,
     });
 
-    server.create('ride', {
+    this.server.create('ride', {
       name: 'Chelsea',
       start: new Date(2016, 11, 25, 10, 15),
       end: new Date(2016, 11, 25, 12, 0),
@@ -37,30 +42,33 @@ moduleForAcceptance('Acceptance | reports', {
       overridable: true,
     });
 
-    server.create('ride', { enabled: false });
+    this.server.create('ride', { enabled: false });
 
-    server.create('ride', {
+    this.server.create('ride', {
       name: 'Assata',
       distance: 100,
     });
 
-    server.create('ride', { combinedWith: edwardRide });
+    this.server.create('ride', { combinedWith: edwardRide });
 
     // This ride should not display because it’s in the future.
-    server.create('ride', {
+    this.server.create('ride', {
       name: 'Future',
       start: new Date(new Date().getTime() + 1000 * 60 * 60 * 24),
       end: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 + 1000),
     });
-  },
-});
+  });
 
-test('submit a report for a ride', function(assert) {
-  authenticateSession(this.application);
+  hooks.afterEach(function() {
+    let toasts = this.owner.lookup('service:paperToaster');
+    toasts.get('queue').forEach(toast => toasts.cancelToast(toast));
+  });
 
-  page.visit();
+  test('submit a report for a ride', async function(assert) {
+    authenticateSession(this.application);
 
-  andThen(function() {
+    await page.visit();
+
     assert.equal(shared.title, 'Ride report · Prison Rideshare');
 
     assert.equal(page.rides.length, 2, 'expected two rides to choose from');
@@ -73,19 +81,17 @@ test('submit a report for a ride', function(assert) {
       page.rides[1].label,
       'Tue, Dec 27 at 5:00p to Fort Leavenworth'
     );
-  });
 
-  page.distance.fillIn(75);
-  page.rides[0].choose();
-  page.foodExpenses.fillIn(25.5);
-  page.carExpenses.fillIn(52.05);
-  page.notes.fillIn('These r the notes');
-  page.donation.click();
+    await page.distance.fillIn(75);
+    await page.rides[0].choose();
+    await page.foodExpenses.fillIn(25.5);
+    await page.carExpenses.fillIn(52.05);
+    await page.notes.fillIn('These r the notes');
+    await page.donation.click();
 
-  page.submitButton.click();
+    await page.submitButton.click();
 
-  andThen(function() {
-    const changedRide = server.db.rides[1];
+    const changedRide = this.server.db.rides[1];
 
     assert.equal(changedRide.distance, 75);
     assert.equal(changedRide.foodExpenses, 2550);
@@ -97,40 +103,36 @@ test('submit a report for a ride', function(assert) {
 
     assert.equal(shared.toast.text, 'Your report was saved');
   });
-});
 
-test('a fallback shows when no rides need a report', function(assert) {
-  authenticateSession(this.application);
+  test('a fallback shows when no rides need a report', async function(assert) {
+    authenticateSession(this.application);
 
-  page.visit();
+    await page.visit();
 
-  page.distance.fillIn(75);
-  page.rides[1].choose();
-  page.submitButton.click();
+    await page.distance.fillIn(75);
+    await page.rides[1].choose();
+    await page.submitButton.click();
 
-  page.distance.fillIn(75);
-  page.rides[0].choose();
-  page.submitButton.click();
+    await page.distance.fillIn(75);
+    await page.rides[0].choose();
+    await page.submitButton.click();
 
-  andThen(function() {
     assert.ok(
       page.noRides.isVisible,
       'expected there to be no rides to report on'
     );
   });
-});
 
-test('submitting a report clears the form', function(assert) {
-  page.visit();
+  test('submitting a report clears the form', async function(assert) {
+    await page.visit();
 
-  page.distance.fillIn(75);
-  page.rides[0].choose();
-  page.foodExpenses.fillIn(25.5);
-  page.notes.fillIn('These r the notes');
+    await page.distance.fillIn(75);
+    await page.rides[0].choose();
+    await page.foodExpenses.fillIn(25.5);
+    await page.notes.fillIn('These r the notes');
 
-  page.submitButton.click();
+    await page.submitButton.click();
 
-  andThen(function() {
     assert.equal(currentURL(), '/reports/new');
     assert.equal(
       page.distance.value,
@@ -144,44 +146,36 @@ test('submitting a report clears the form', function(assert) {
     );
     assert.equal(page.notes.value, '', 'expected the notes to be empty');
   });
-});
 
-test('partially completing a report and changing the ride doesn’t erase the values', function(assert) {
-  const longReport = 'This is a very long report I would be sad to lose.';
+  test('partially completing a report and changing the ride doesn’t erase the values', async function(assert) {
+    const longReport = 'This is a very long report I would be sad to lose.';
 
-  page.visit();
+    await page.visit();
 
-  page.notes.fillIn(longReport);
+    await page.notes.fillIn(longReport);
 
-  page.rides[0].choose();
+    await page.rides[0].choose();
 
-  andThen(() => {
+    assert.equal(page.notes.value, longReport);
+
+    await page.rides[1].choose();
+
     assert.equal(page.notes.value, longReport);
   });
 
-  page.rides[1].choose();
+  test('submitting a report without choosing a ride displays an error', async function(assert) {
+    await page.visit();
+    await page.notes.fillIn('I cannot find my ride');
+    await page.submitButton.click();
 
-  andThen(() => {
-    assert.equal(page.notes.value, longReport);
-  });
-});
-
-test('submitting a report without choosing a ride displays an error', function(assert) {
-  page.visit();
-  page.notes.fillIn('I cannot find my ride');
-  page.submitButton.click();
-
-  andThen(() => {
     assert.equal(shared.toast.text, 'Please choose a ride');
     assert.equal(page.notes.value, 'I cannot find my ride');
   });
-});
 
-test('a ride that is not donatable doesn’t show the donation checkbox, same for overridable and car expenses', function(assert) {
-  page.visit();
-  page.rides[1].choose();
+  test('a ride that is not donatable doesn’t show the donation checkbox, same for overridable and car expenses', async function(assert) {
+    await page.visit();
+    await page.rides[1].choose();
 
-  andThen(() => {
     assert.ok(
       page.donation.isHidden,
       'expected the donation checkbox to be hidden when a ride is not donatable'
@@ -191,25 +185,23 @@ test('a ride that is not donatable doesn’t show the donation checkbox, same fo
       'expected the car expenses field to be hidden when a ride is not overridable'
     );
   });
-});
 
-test('a failure to save keeps the values and displays an error', function(assert) {
-  server.patch(
-    '/rides/:id',
-    () => {
-      return {};
-    },
-    422
-  );
+  test('a failure to save keeps the values and displays an error', async function(assert) {
+    this.server.patch(
+      '/rides/:id',
+      () => {
+        return {};
+      },
+      422
+    );
 
-  page.visit();
+    await page.visit();
 
-  page.distance.fillIn(75);
-  page.rides[0].choose();
+    await page.distance.fillIn(75);
+    await page.rides[0].choose();
 
-  page.submitButton.click();
+    await page.submitButton.click();
 
-  andThen(function() {
     assert.equal(shared.toast.text, 'There was an error saving your report!');
 
     assert.equal(currentURL(), '/reports/new');
