4168fe7c03babcb0200cfdcfb3efa9001bdc6520
Buck Doyle <b@chromatin.ca>
2018-12-18 21:07:38 -0600

    Remove Mirage shutdown

--- tests/helpers/destroy-app.js
+++ tests/helpers/destroy-app.js
@@ -2,6 +2,5 @@ import { run } from '@ember/runloop';
 
 export default function destroyApp(application) {
   run(application, 'destroy');
-  server.shutdown();
   localStorage.removeItem('person-token');
 }
