6301fbca4e7a97ccbeb1f2128600561af8d0045b
Buck Doyle <b@chromatin.ca>
2019-05-10 16:06:13 -0500

    Add silencing for some ED-blocked deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,5 +1,16 @@
 /* global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [{ handler: 'silence', matchId: 'ember-component.send-action' }],
+  workflow: [
+    { handler: 'silence', matchId: 'ember-component.send-action' },
+
+    // Ember Data is triggering this, currently stuck at 3.1 due to https://github.com/samselikoff/ember-cli-mirage/pull/1613
+    {
+      handler: 'silence',
+      matchId: 'ember-views.curly-components.jquery-element',
+    },
+
+    // Also Ember Data
+    { handler: 'silence', matchId: 'ember-env.old-extend-prototypes' },
+  ],
 };
