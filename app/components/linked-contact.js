8297fde396f38b70c3285d2308801f40bb036b5f
Buck Doyle <b@chromatin.ca>
2019-07-21 12:46:02 -0500

    Change name of linked-contact computed property
    The property having the same name as the component (though
    different casing) seems to have caused a problem 😳

--- app/components/linked-contact.js
+++ app/components/linked-contact.js
@@ -6,7 +6,7 @@ import { htmlSafe } from '@ember/string';
 export default Component.extend({
   tagName: 'span',
 
-  linkedContact: computed('contact', function() {
+  link: computed('contact', function() {
     const phonePattern = /(\([0-9]{3}\)\s?|[0-9]{3}-?\s?)[0-9]{3}-?\s?[0-9]{4}/g;
     const contact = this.get('contact');
 
