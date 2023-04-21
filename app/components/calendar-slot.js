backspace/prison-rideshare-ui

commit c300248dc6f8c7e7eb008bf39c3d4625eb7a3209

Change full calendar slots to be hidden
This has caused confusion. Is it also confusing for ones
in the past to be disabled? 🤔

app/components/calendar-slot.js
@@ -29,6 +29,10 @@ export default Component.extend({
     );
   }),
 
+  hidden: computed('slot.isNotFull', function() {
+    return !this.get('slot.isNotFull');
+  }),
+
   disabled: computed('slot.{isNotFull,start}', 'toggle.isRunning', function() {
     const isNotFull = this.get('slot.isNotFull');
     const start = this.get('slot.start');

tests/acceptance/calendar-test.js
@@ -148,7 +148,7 @@ module('Acceptance | calendar', function(hooks) {
     );
   });
 
-  test('full slots show as full and can’t be committed to', async function(assert) {
+  test('full slots are hidden', async function(assert) {
     this.server.post('/commitments', function() {
       assert.ok(false, 'expected no commitment to be created for a full slot');
     });
@@ -159,15 +159,8 @@ module('Acceptance | calendar', function(hooks) {
     await page.visit({ month: '2117-12', token: 'MAGIC??TOKEN' });
 
     assert.ok(
-      page.days[9].slots[1].isDisabled,
-      'expected the full slot to be disabled'
-    );
-
-    await page.days[9].slots[1].click();
-
-    assert.notOk(
-      page.days[9].slots[1].isCommittedTo,
-      'expected the slot to not be committed-to'
+      page.days[9].slots[1].isHidden,
+      'expected the full slot to be hidden'
     );
   });
 

app/styles/calendar.scss
@@ -21,6 +21,10 @@
 
   .slot {
     transform: scale(0.7);
+
+    &.hidden {
+      display: none;
+    }
   }
 
   .ember-power-calendar-weekdays, .ember-power-calendar-weekday {

app/templates/components/calendar-slot.hbs
@@ -1,4 +1,4 @@
-<div class='slot'>
+<div class='slot {{if hidden 'hidden'}}'>
   {{#if count}}
     <span class='hours'>{{timespan}}</span>
     <button class='count {{if (gt slot.commitments.length 0) 'committed-to'}}' onClick={{action setViewingSlot slot}}>{{capacity}}</button>

tests/pages/calendar.js
@@ -133,6 +133,8 @@ export default create({
       isDisabled: getter(function() {
         return this.disabledAttribute === 'disabled';
       }),
+
+      isHidden: hasClass('hidden'),
     }),
   }),
 

commit b1b48be3e02f7303df911bbb6b89b1950cd76018

Change slots to not be hidden if one has committed
The “fix” in c300248 caused more confusion. This was masked
in development because lately we have only been having slots
with a count of 1, they were disappearing whenever someone
chose them, which meant they couldn’t uncommit!

app/components/calendar-slot.js
@@ -29,8 +29,8 @@ export default Component.extend({
     );
   }),
 
-  hidden: computed('slot.isNotFull', function() {
-    return !this.get('slot.isNotFull');
+  hidden: computed('slot.isNotFull', 'isCommittedTo', function() {
+    return !this.get('slot.isNotFull') && !this.get('isCommittedTo');
   }),
 
   disabled: computed('slot.{isNotFull,start}', 'toggle.isRunning', function() {

tests/acceptance/calendar-test.js
@@ -39,7 +39,7 @@ module('Acceptance | calendar', function(hooks) {
     const committedSlot = this.server.create('slot', {
       start: new Date(2117, 11, 4, 17, 30),
       end: new Date(2117, 11, 4, 20),
-      count: 3,
+      count: 1,
     });
 
     this.toCommitSlot = this.server.create('slot', {
@@ -160,7 +160,7 @@ module('Acceptance | calendar', function(hooks) {
     );
   });
 
-  test('full slots are hidden', async function(assert) {
+  test('full and committed-to slots show as such and full slots from others are hidden', async function(assert) {
     this.server.post('/commitments', function() {
       assert.ok(false, 'expected no commitment to be created for a full slot');
     });
@@ -530,8 +530,8 @@ module('Acceptance | calendar', function(hooks) {
 
     assert.equal(
       page.days[3].slots[0].count.text,
-      '2/3',
-      'expected two people to show for the slot out of a maximum of three'
+      '2/1',
+      'expected two people to show for the slot out of a maximum of one'
     );
     assert.ok(
       page.days[3].slots[0].count.isCommittedTo,