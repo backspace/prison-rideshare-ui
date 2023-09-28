3434e5483b2dac76ca2097faec1435e9feaadef1
Buck Doyle <b@chromatin.ca>
2023-09-27 21:36:25 -0500

    Update deprecation workflow config

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,10 +3,14 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
+    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+    { handler: 'silence', matchId: 'ember-utils.try-invoke' },
     { handler: 'silence', matchId: 'ember-modifier.use-modify' },
     { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
     { handler: 'silence', matchId: 'ember-modifier.no-element-property' },
     { handler: 'silence', matchId: 'ember-modifier.use-destroyables' },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
 
     // Is this comment still relevant?
     // This seems to be from paper-data-table, though I can’t find exactly where 🤔
@@ -16,5 +20,7 @@ self.deprecationWorkflow.config = {
     // Is this comment still relevant?
     // Despite adding a setter for child-mixin#parentComponent, the warning still occurs, maybe because of https://github.com/miguelcobain/ember-paper/blob/faa243a68943ce8406b82f72d21ec64e2484c3b9/addon/templates/components/paper-radio-group.hbs#L10
     { handler: 'silence', matchId: 'computed-property.override' },
+    { handler: 'silence', matchId: 'ember-metal.computed-deep-each' },
+    { handler: 'silence', matchId: 'ember-cli-page-object.is-property' },
   ],
 };
