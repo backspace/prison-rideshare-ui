33e3c228ccf81de55324c582df73cc2532a8bdb8
Buck Doyle <b@chromatin.ca>
2023-09-30 11:13:00 -0500

    Update deprecation workflow

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,19 +3,26 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+    { handler: 'silence', matchId: 'ember-global' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    { handler: 'silence', matchId: 'implicit-injections' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
     { handler: 'silence', matchId: 'ember-utils.try-invoke' },
+    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+    { handler: 'silence', matchId: 'ensure-safe-component.string' },
     { handler: 'silence', matchId: 'ember-modifier.use-modify' },
     { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
     { handler: 'silence', matchId: 'ember-modifier.no-element-property' },
     { handler: 'silence', matchId: 'ember-modifier.use-destroyables' },
-    { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
-    { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
 
     // Is this comment still relevant?
     // This seems to be from paper-data-table, though I can’t find exactly where 🤔
     { handler: 'silence', matchId: 'ember-component.send-action' },
-    { handler: 'silence', matchId: 'ensure-safe-component.string' },
+    { handler: 'silence', matchId: 'ember.link-to.tag-name' },
 
     // Is this comment still relevant?
     // Despite adding a setter for child-mixin#parentComponent, the warning still occurs, maybe because of https://github.com/miguelcobain/ember-paper/blob/faa243a68943ce8406b82f72d21ec64e2484c3b9/addon/templates/components/paper-radio-group.hbs#L10
