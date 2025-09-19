708b69e74357e30c1a5b4347558ef7bccf168040
Buck Doyle <b@chromatin.ca>
2025-09-19 11:34:10 +0200

    Remove Travis files

--- .travis/deploy-pull-request.sh
+++
@@ -1,8 +0,0 @@
-./.travis/prepare-ssh-key.sh
-
-export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' | tr '[:upper:]' '[:lower:]'`
-
-export API_HOST=https://rideshare-api.barnonewpg.org
-export SOCKET_HOST=wss://rideshare-api.barnonewpg.org
-ember deploy pull-request-production --activate
-DEPLOYMENT_EXIT_CODE=$? ENVIRONMENT=production ./.travis/update-github-status.sh
--- .travis/prepare-ssh-key.sh
+++
@@ -1,7 +0,0 @@
-openssl aes-256-cbc -K $encrypted_ce91012c77dd_key -iv $encrypted_ce91012c77dd_iv -in .travis/keys.tar.enc -out .travis/keys.tar -d
-tar xfv .travis/keys.tar
-chmod -R 700 .travis
-
-eval "$(ssh-agent -s)" #start the ssh agent
-ssh-add .travis/$KEY_FILENAME
-ssh-keyscan corepoint.chromatin.ca >> ~/.ssh/known_hosts
