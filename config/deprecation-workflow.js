d73eecd6c9697768e7984dd075ad57448f4fd2b6
Buck Doyle <b@chromatin.ca>
2025-09-21 14:45:29 +0200

    Add more deprecation silencings

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -4,6 +4,8 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: 'silence', matchId: 'ember-global' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
+    { handler: 'silence', matchId: 'ember-utils.try-invoke' },
     {
       handler: 'silence',
       matchId: 'deprecated-run-loop-and-computed-dot-access',
