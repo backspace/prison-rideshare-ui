cd1cf4e8e4126a64b31e6df9ab2bb2336cafbe80
Buck Doyle <b@chromatin.ca>
2025-09-21 16:10:28 +0200

    Add missed classic decorator

--- tests/unit/utils/reimbursement-collection-test.js
+++ tests/unit/utils/reimbursement-collection-test.js
@@ -1,5 +1,6 @@
 /* eslint-disable ember/no-classic-classes */
 import EmberObject from '@ember/object';
+import classic from 'ember-classic-decorator';
 import ReimbursementCollection from 'prison-rideshare-ui/utils/reimbursement-collection';
 
 import { module, test } from 'qunit';
@@ -13,6 +14,7 @@ module('Unit | Utility | reimbursement collection', function () {
     today.getMonth() + 1
   }/${today.getDate()}/${today.getFullYear()}`;
 
+  @classic
   class FakeReimbursement extends EmberObject {
     ride = ride;
     date = reimbursementDate;
