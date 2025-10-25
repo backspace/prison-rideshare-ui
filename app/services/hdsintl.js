843bdfc2081f26263fc37b0665cb2f27e3cee598
Buck Doyle <b@chromatin.ca>
2025-10-25 20:55:56 +0100

    Update stubbed HdsIntl service
    Removing it does indeed break everything ¯\_(ツ)_/¯

--- app/services/hdsintl.js
+++ app/services/hdsintl.js
@@ -1,10 +1,9 @@
-// import HdsIntlService from '@hashicorp/design-system-components/services/hds-intl';
 import Service from '@ember/service';
 
-// FIXME is this needed, and the initialiser? Maybe not if the t helper is hacked
+// This is a stub to provide the translation helper, things break without it despite this not needing intl
 export default class HdsIntl extends Service {
   t(key, options) {
-    const { default: defaultString, ...restOptions } = options;
+    const { default: defaultString } = options;
     return defaultString || key;
   }
 }
