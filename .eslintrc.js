5573eb3a4cc969de98fe5dad9b7d20b9790262eb
Buck Doyle <b@chromatin.ca>
2025-12-23 21:59:12 -0600

    Add results of ember-cli-update to 4.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
       },
     },
     {
-      // Test files:
+      // test files
       files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
     },
