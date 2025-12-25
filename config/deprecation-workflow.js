bb4dea44e26c747bd8e684eafdba5b5d7f15510f
Buck Doyle <b@chromatin.ca>
2025-12-25 13:09:41 -0600

    Add silencing of fake ember-string deprecation

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,5 +2,5 @@
 
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [],
+  workflow: [{ handler: 'silence', matchId: 'ember-string.add-package' }],
 };
